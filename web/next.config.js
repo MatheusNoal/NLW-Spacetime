c2802b0b734bc903e1946ccd9f41c23962a790d7
matheusnoal <matheusnoal2@gmail.com>
2023-05-23 18:46:14 -0300

    nwl

---
+++ web/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['avatars.githubusercontent.com', 'http://localhost:3000/'],
+  },
+}
+
+module.exports = nextConfig
